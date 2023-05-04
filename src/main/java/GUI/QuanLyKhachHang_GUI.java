3728e893c0615d1fd535b408b28b5971a8da10b3
khang0710 <117827755+khang0710@users.noreply.github.com>
2023-05-04 07:21:05 +0700

    .

--- src/main/java/GUI/QuanLyKhachHang_GUI.java
+++ src/main/java/GUI/QuanLyKhachHang_GUI.java
@@ -425,7 +425,7 @@ public class QuanLyKhachHang_GUI extends javax.swing.JPanel {
                 .addGap(346, 346, 346))
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 95, Short.MAX_VALUE))
+                .addGap(0, 93, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
